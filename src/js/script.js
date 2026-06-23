3b7e39f021ca01b617df9e0684d2d26f612003a7
academyHH <mlucaninov408@gmail.com>
2026-06-23 15:59:58 +0300

    Add animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import JustValidate from "just-validate";
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -67,44 +68,104 @@ try {
 	contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
 } catch (e) {}
 
-try{
-const validator = new JustValidate('form');
+try {
+	const validatorTouch = new JustValidate(".touch__form");
 
-validator
-  .addField('#name', [
-    {
-      rule: 'required',
-	  errorMessage: 'Please fill the name',
-    },
-    {
-      rule: 'minLength',
-      value: 2,
-    },
-  ])
-    .addField('#email', [
-    {
-      rule: 'required',
-	  errorMessage: 'Please fill the email',
-    },
-    {
-      rule: 'email',
-    },
-  ])
-      .addField('#question', [
-    {
-      rule: 'required',
-	  errorMessage: 'Please fill the question',
-    },
-    {
-      rule: 'minLength',
-	  value: 5,
-    },
-  ])
-        .addField('#checkbox', [
-    {
-      rule: 'required',
-	  errorMessage: 'Please accept the terms',
-    },
-  ])
-} catch (e) {
-}
+	validatorTouch
+		.addField("#name", [
+			{
+				rule: "required",
+				errorMessage: "Please fill the name",
+			},
+			{
+				rule: "minLength",
+				value: 2,
+				errorMessage: "Minimum 2 chars!",
+			},
+		])
+		.addField("#email", [
+			{
+				rule: "required",
+			},
+			{
+				rule: "email",
+			},
+		])
+		.addField(
+			"#question",
+			[
+				{
+					rule: "required",
+				},
+				{
+					rule: "minLength",
+					value: 5,
+				},
+			],
+			{
+				errorsContainer: document
+					.querySelector("#question")
+					.parentElement.querySelector(".error-message"),
+			}
+		)
+		.addField(
+			"#checkbox",
+			[
+				{
+					rule: "required",
+				},
+			],
+			{
+				errorsContainer: document
+					.querySelector("#checkbox")
+					.parentElement.parentElement.querySelector(".checkbox-error-message"),
+			}
+		)
+		.onSuccess((event) => {
+			const form = event.currentTarget;
+			const formData = new FormData(form);
+
+			fetch("https://httpbin.org/post", {
+				method: "POST",
+				body: formData,
+			}).then((response) => response.json()).then((data) => {
+				console.log('Success:', data);
+				form.reset();
+			});
+		});
+} catch (e) {}
+
+try {
+	const validatorFooter = new JustValidate(".footer__form");
+
+	validatorFooter
+		.addField(
+			"#footer__email",
+			[
+				{
+					rule: "required",
+				},
+				{
+					rule: "email",
+				},
+			],
+			{
+				errorsContainer: document
+					.querySelector("#footer__email")
+					.parentElement.querySelector(".email-error-message"),
+			}
+		)
+		.addField(
+			"#footer__checkbox",
+			[
+				{
+					rule: "required",
+				},
+			],
+			{
+				errorsContainer: document
+					.querySelector("#footer__checkbox")
+					.parentElement.parentElement.querySelector(".check-error-message"),
+			}
+		);
+} catch (e) {}
